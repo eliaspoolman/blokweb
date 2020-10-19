25a154dd1a4b8a8ec1d802e347fe4178d73cb3bd
Elias Poolman <eliaspoolman@gmail.com>
2020-10-20 01:14:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
+
+
 // JavaScript Document
 var menubutton = document.querySelector("header nav");
 
 var closebutton = document.querySelector(".closemenu");
 
 function toonmenu() {
-  var element = document.getElementById("menustripe");
-  element.classList.toggle("menustripe");
+    var element = document.getElementById("menustripe");
+    element.classList.toggle("menustripe");
 }
 
 menubutton.addEventListener("click", toonmenu);
