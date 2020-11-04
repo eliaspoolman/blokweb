445925b1ab446b33d7b4e6fb8d3aee9f3239fcd9
Elias Poolman <eliaspoolman@gmail.com>
2020-11-04 16:25:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,20 @@ menubutton.addEventListener("click", toonmenu);
 
 closebutton.addEventListener("click", toonmenu);
 
+// Afspelen van typen codeeditor
+
+var editorcontainer = document.querySelector(".editorcontainer");
+
+function typeeditor() {
+    var element = document.getElementsByClassName("editor");
+    
+    for (var i = 0; i < element.length; i++) {
+        element[i].classList.add("typeneditor");
+    }
+}
+
+editorcontainer.addEventListener("mouseenter", typeeditor);
+
 //Insliden items about page
 
 var paymentsstack = document.querySelector(".paymentsstack");
