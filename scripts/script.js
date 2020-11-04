fbad9fbb3fa5c9cdae3600855dcdb8f1a4f26533
Elias Poolman <eliaspoolman@gmail.com>
2020-11-04 15:13:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-
-
 // JavaScript Document
+
+//Menu werking
 var menubutton = document.querySelector("header nav");
 
 var closebutton = document.querySelector(".closemenu");
@@ -13,3 +13,20 @@ function toonmenu() {
 menubutton.addEventListener("click", toonmenu);
 
 closebutton.addEventListener("click", toonmenu);
+
+//Insliden items about page
+
+var paymentsstack = document.querySelector(".paymentsstack");
+
+function toonplaatjes() {
+    var element = document.getElementsByClassName(".slidein");
+    
+    for (var i = 0; i < element.length; i++) {
+        element[i].classList.toggle("hidden");
+        element[i].classList.toggle("fade-in-element");
+    }
+
+}
+
+paymentsstack.addEventListener("mouseenter", toonplaatjes);
+
